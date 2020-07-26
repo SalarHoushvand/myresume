9f23087b7d57cea70c9aa8b56417d3794e8d51ae
Salar Houshvand <salarhoushvand@yahoo.com>
2020-07-26 01:16:24 -0400

    updated

--- index.js
+++ index.js
@@ -41,3 +41,13 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
